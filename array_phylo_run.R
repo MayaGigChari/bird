825789356628ac86f472d553c7e9a0afbe7dfd38
MayaGigChari <mchari@ucla.edu>
2022-10-11 22:42:57 -0700

    added new files

---
+++ array_phylo_run.R
@@ -0,0 +1,28 @@
+#script for one run of a ph_comstruct; to run in parallel. 
+
+
+#install.packages("phylocomr", repos = "http://cran.us.r-project.org")
+#install.packages("ape", repos = "http://cran.us.r-project.org")
+library(phylocomr)
+library(ape)
+
+args = commandArgs(trailingOnly=TRUE) #not sure what this part does, but I think it allows us to accept args when the script is sent. 
+print(args) #prints the arguments which here should be equivalent to each SGE_TASK_ID
+n = as.integer(args[1]) #makes the SGE_TASK_ID an integer
+
+
+sample_tree_cluster<-read.tree(file = "sample_tree_for_cluster.tre")
+
+full_tree_cluster<-read.tree(file = "full_tree_for_cluster.tre")
+
+df_touse_cluster<-data.frame(sample = "species", occurrence = 1, names = sample_tree_cluster$tip.label)
+res_sample_cluster<- ph_comstruct(sample = df_touse_cluster, phylo = full_tree_cluster, randomizations = 1)
+typeof(res_sample_cluster)
+#write.table(res_sample_cluster, file = "sample_phcomstruct_output") #better to have csv maybe, easier to manipulate?
+
+path <- "/u/home/m/mchari/output_phylo/"
+fileName = paste(path, "phcom_out",n,".csv", sep = '')
+write.csv(res_sample_cluster, file = fileName) #this i believe is the output file. I think it's maybe better than table?
+
+
+
---
+++ submit_array_phylo.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#$ -cwd #uses current working directory
+# error = Merged with joblog
+#$ -o /u/home/m/mchari/joblog_$JOB_ID/joblog.$JOB_ID.$TASK_ID #creates a file called joblog.jobidnumber.taskidnumber to write to. 
+#$ -j y 
+#$ -l h_rt=0:30:00,h_data=2G #requests 30 minutes, 2GB of data (per core)
+#$ -pe shared 2 #requests 2 cores
+# Email address to notify
+#$ -M $USER@mail #don't change this line, finds your email in the system 
+# Notify when
+##$ -m bea #sends you an email (b) when the job begins (e) when job ends (a) when job is aborted (error)
+#$ -t 1-1000:1 # 1 to 10, with step size of 1
+
+# load the job environment:
+. /u/local/Modules/default/init/modules.sh
+module load R
+
+echo ${SGE_TASK_ID}
+
+# run julia code
+echo Running phylo_code for n = ${SGE_TASK_ID} #prints this quote to joblog.jobidnumber
+Rscript array_phylo_run.R ${SGE_TASK_ID}  > /u/home/m/mchari/joblog_$JOB_ID/output.$JOB_ID.${SGE_TASK_ID}
