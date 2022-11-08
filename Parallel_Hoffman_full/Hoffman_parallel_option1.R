d960daf9ab087f99d95262386e9692d197b83b92
Maya Chari <mchari@g.ucla.edu>
2022-11-07 23:34:32 -0800

    minor changes

--- Parallel_Hoffman_full/Hoffman_parallel_option1.R
+++ Parallel_Hoffman_full/Hoffman_parallel_option1.R
@@ -4,7 +4,7 @@
 #define global variables/ variables to change. 
 
 #number of randomizations
-n_rand = 5
+n_rand = 100
 
 
 #create new directories to hold outputs
@@ -70,6 +70,7 @@ print("trees read")
 df_touse_cluster<-data.frame(sample = "species", occurrence = 1, names = sample_tree_cluster$tip.label)
 res_sample_cluster<- ph_comstruct(sample = df_touse_cluster, phylo = full_tree_cluster, randomizations = 1)
 typeof(res_sample_cluster)
+
 #write.table(res_sample_cluster, file = "sample_phcomstruct_output") #better to have csv maybe, easier to manipulate?
 
 print("ph_comstruct successfully applied")
