0a66274d2cab009ef84768d2101249d24f96cff9
Maya Chari <mchari@g.ucla.edu>
2023-01-16 12:31:32 -0800

    changed variable

--- Parallel_Hoffman_full/sample_tree_creator.R
+++ Parallel_Hoffman_full/sample_tree_creator.R
@@ -6,14 +6,14 @@ library(ape)
 #read parent tree
 full_fish_phylo<-read.tree(file = "full_tree_for_cluster.tre")
 
-tree_sizes = list(10,50,100,200,1000)
+tree_sizes = list(25,350,500,700,900) #this has been edited. Can accomodate any sizes 
 
 for(i in tree_sizes)
 {
   species_names_2<-full_fish_phylo$tip.label
   random_sample_2<-sample(species_names_2,i)
   random_sample_2l<- c(random_sample_2)
-  sample_tree_2<-keep.tip(full_fish_phylo, random_sample_2l) #maybe try to print this tree somehow later. 
+  sample_tree_2<-keep.tip(full_fish_phylo, random_sample_2l) #maybe try to prit this tree somehow later. 
   filename = paste("sample_tree_cluster", i, sep = "")
   write.tree(sample_tree_2, file = filename)
 }
