3d2a83446fc0ec7c01051750e6e8c69081f6ce6e
Maya Chari <mchari@g.ucla.edu>
2023-02-16 10:45:17 -0800

    added files for handcalc

--- Parallel_Hoffman_full/1000_sims_trees.R
+++ Parallel_Hoffman_full/1000_sims_trees.R
@@ -2,6 +2,7 @@ library(ape)
 library(phylocomr)
 library(tictoc)
 
+
 full_tree<-read.tree("full_tree_for_cluster.tre")
 species_names<-full_tree$tip.label
 
@@ -20,7 +21,7 @@ metric_extract<-function(data, metrics) #vector data is in the form of ph_comstr
   if(metrics == "mpd_random")
     name = "mpd.rnd"
   else if(metrics == "mpd_true")
-    name = "mpd.true"
+    name = "mpd"
   else if(metrics == "mntd")
     name = "mntd.rnd"
   metric_list<-unlist(data)
@@ -34,6 +35,11 @@ CI_calc<- function(mpd_metrics_list)
   lower_bound<- quantile(mpd_metrics_unlist,.025)
   upper_bound<-quantile(mpd_metrics_unlist,0.975)
   return(c(lower_bound, upper_bound))
+}
+
+tree_writer<- function(tree_list)
+{
+  write.tree(tree_list)
 }
 
 tic()
@@ -50,16 +56,37 @@ mpd_true_metrics<-lapply(res_sample,metric_extract, metrics = "mpd_true")
 write.csv(res_sample, file = "pd_calcs_sim.csv")
 # the tic toc functions just give us some measure of time.. this program took 11 hours to run!
 
-# get the 95% confidence intervals
-CI<- CI_calc(mpd_metrics) #calculate CI's
+#visualizing the distribution of the true mpd values for all our random trees. 
 
+mpd_vals_hist<-as.numeric((unlist(mpd_true_metrics)))
+hist(mpd_vals_hist, main = "mpd's of 1000 random 5-tip trees",xlab = "mpd")
+
+# get the 95% confidence intervals
+CI_rand<- CI_calc(mpd_rand_metrics) #calculate CI's
+CI_true<-CI_calc(mpd_true_metrics)
 #find pd data that is outside the extremes of the CI and correlated tree data
 
-pd_below_range_index<- which(mpd_metrics < CI[1]) #indexes the same through the pd vector and the trees vector. 
-pd_above_range_index<- which(mpd_metrics > CI[2])
+mpd_below_range_index_true<- which(mpd_true_metrics < CI[1]) #indexes the same through the pd vector and the trees vector. 
+mpd_above_range_index_true<- which(mpd_true_metrics > CI[2])
 
-trees_above_CI<- trees[c(pd_above_range_index)]
-trees_below_CI<-trees[c(pd_below_range_index)]
+trees_above_CI_true<- trees[c(mpd_above_range_index_true)]
+trees_below_CI_true<-trees[c(mpd_below_range_index_true)]
+
+mpd_above_CI_true<- mpd_true_metrics[c(mpd_above_range_index_true)]
+mpd_below_CI_true<-mpd_true_metrics[c(mpd_below_range_index_true)]
 
 
+for(i in 1:length(trees_above_CI_true))
+{
+  write.tree(trees_above_CI_true[i], file = paste("above_CI_data/tree_above_CI",i, sep = ''))
+  write.csv(mpd_above_CI_true[i], file = paste("above_CI_data/mpd_above_CI",i,sep = ''))
+}
 
+for(i in 1:length(trees_below_CI_true))
+{
+  write.tree(trees_below_CI_true[i], file = paste("below_CI_data/tree_below_CI",i, sep = ''))
+  write.csv(mpd_below_CI_true[i], file = paste("below_CI_data/mpd_below_CI",i,sep = ''))
+}
+
+
+write.csv(as.numeric(unlist(mpd_true_metrics)),file= "mpd_metrics.csv",row.names=F)
