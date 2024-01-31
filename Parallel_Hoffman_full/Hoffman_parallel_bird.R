ec943de6286e9e6798a0ff0233e57d7ea538a21b
Maya Chari <mchari@g.ucla.edu>
2024-01-31 11:54:44 -0800

    edited script

--- Parallel_Hoffman_full/Hoffman_parallel_bird.R
+++ Parallel_Hoffman_full/Hoffman_parallel_bird.R
@@ -8,25 +8,26 @@
 #sample_tree_cluster (right now specific to birds)
 #cophen_read (right now specific to birds)
 #path_to_output
+#take advantage of the fact that this script is in the working directory 
 
 n_rand = 1 #want 2  randomizations for each tree size. internally each script will produce 500 random values. 
 min_null_tree_size = 5
-max_null_tree_size = 600
+max_null_tree_size = 100
 tree_size_increment = 5
-Clade = "birds" #need to make it so this can be any clade. 
+Clade = "squamate" #need to make it so this can be any clade. 
 num_randomizations = 1000
-path_to_output <- paste("/u/home/m/mchari/bird/Parallel_Hoffman_full/", Clade, sep = "") #this is fairly universal. 
+username = "mchari" #here edit to make hoffman 2 username.
 
-params = seq(min_null_tree_size, max_null_tree_size, by=tree_size_increment) #this is a list of tree sizes. 
-#params2 = seq(500,600, by = 100)
+#above is all local variable declarations. 
 
-
-
-
-new_libPaths = .libPaths(c('/u/home/m/mchari/R',.libPaths()))
+path_to_output <- paste(getwd(), Clade, sep = "") #this is fairly universal. 
+new_libPaths = .libPaths(c(paste('/u/home/m/',username, '/R', sep = ""),.libPaths())) #library path for cluster
 .libPaths(new_libPaths)
 
-print("path created")
+print("paths created")
+
+params = seq(min_null_tree_size, max_null_tree_size, by=tree_size_increment) 
+
 
 #this seems to have an issue. 
 #create new directories to hold outputs
@@ -63,13 +64,26 @@ start_time<-format(Sys.time(), "%H:%M:%S") #identify start time of script
 
 #install.packages("picante",repos = "http://cran.us.r-project.org")
 #install.packages("dplyr",repos = "http://cran.us.r-project.org")
+#install.packages("ape",repos = "http://cran.us.r-project.org")
 #library("picante", lib.loc = .libPaths())
 #library("ape", lib.loc = .libPaths())
-#library("dplyr", lib.loc = )
+#library("dplyr", lib.loc = .libPaths())
+
+#try this and see if it works.
+
+packages_to_install <- c("picante", "dplyr", "ape")
+
+for (package in packages_to_install) {
+  if (!require(package, character.only = TRUE)) {
+    install.packages(package, repos = "http://cran.us.r-project.org", lib = new_libPaths)
+  }
+}
 
 library(picante)
 library(ape)
 library(dplyr)
+
+#print("libraries successfully loaded)
 
 args = commandArgs(trailingOnly=TRUE) #not sure what this part does, but I think it allows us to accept args when the script is sent. 
 print(args) #prints the arguments which here should be equivalent to each SGE_TASK_ID.
@@ -143,7 +157,7 @@ print(fileIDtimes)
 write.csv(res_sample_cluster, file = fileIDcsv, row.names = FALSE) #this i believe is the output file. I think it's maybe better than table?
 
 #checkpoint
-print("data output")
+print("data output successful")
 
 #stuff for time calculation
 end_time<-format(Sys.time(), "%H:%M:%S") #identify end time of script
