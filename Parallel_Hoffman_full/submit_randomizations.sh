66063300784cde0dfdd29b91c5bd76ec31277aae
Maya Chari <mchari@g.ucla.edu>
2024-02-02 13:57:20 -0800

    updated submission script

--- Parallel_Hoffman_full/submit_randomizations.sh
+++ Parallel_Hoffman_full/submit_randomizations.sh
@@ -1,30 +1,12 @@
-#!/bin/bash
-# Get the directory of the script
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# Construct directory paths relative to the script location
-JOB_LOG_DIR="$SCRIPT_DIR/joblog_$JOB_ID"
-OUTPUT_DIR="$SCRIPT_DIR/joblog_$JOB_ID/output.$JOB_ID"
-
-#$ -cwd
-# error = Merged with joblog
-#$ -o $JOB_LOG_DIR/joblog.$JOB_ID.$TASK_ID
-#$ -j y
-#$ -l h_rt=0:30:00,h_data=2G
-#$ -pe shared 2
-# Email address to notify
-#$ -M $USER@mail
-# Notify when
-##$ -m bea
-#$ -t 1:240
 
 #!/bin/bash
 # Get the directory of the script
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Construct directory paths relative to the script location
-JOB_LOG_DIR="$SCRIPT_DIR/joblog_$JOB_ID"
-OUTPUT_DIR="$SCRIPT_DIR/joblog_$JOB_ID/output.$JOB_ID"
+
+JOB_LOG_DIR="$SCRIPT_DIR/joblog.$JOB_ID"
+OUTPUT_DIR="$SCRIPT_DIR/output.$JOB_ID"
 
 #$ -cwd
 # error = Merged with joblog
