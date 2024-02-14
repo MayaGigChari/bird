fa7c35a1bd744265c3783b91a4cad4e80b6209fa
Maya Chari <mchari@g.ucla.edu>
2024-02-14 11:50:31 -0800

    local changes to libpath

--- Parallel_Hoffman_full/birds/range_gen.R
+++ Parallel_Hoffman_full/birds/range_gen.R
@@ -1,5 +1,15 @@
-#install.packages("sf")
-library(sf)
+
+new_libPaths = .libPaths(c(paste('/u/home/m/',username, '/R', sep = ""),.libPaths())) #library path for cluster
+.libPaths(new_libPaths)
+packages_to_install <- c("sf")
+
+for (package in packages_to_install) {
+  if (!require(package, character.only = TRUE)) {
+    install.packages(package, repos = "http://cran.us.r-project.org", lib = new_libPaths)
+  }
+  library(package)
+}
+
 
 # Read the GeoJSON file
 geojson_file <- "bien_ranges.geojson"
@@ -11,3 +21,5 @@ california_bbox_polygon <- st_as_sfc(st_bbox(california_bbox))
 
 # filters geojson for only those that intersect with bounding box 
 birds_in_california <- bird_ranges[st_intersects(bird_ranges, california_bbox_polygon), ]
+
+print("found birds in california")
