ca81fe2e3c4b62f9942f55f0ef86f0747cd87a60
Maya Chari <mchari@g.ucla.edu>
2024-02-21 13:48:49 -0800

    small change

--- Parallel_Hoffman_full/Hoffman_parallel_plant.R
+++ Parallel_Hoffman_full/Hoffman_parallel_plant.R
@@ -9,6 +9,7 @@
 #cophen_read (right now specific to birds)
 #path_to_output
 #take advantage of the fact that this script is in the working directory 
+#note: needed to globus file transfer the cophenetic matrices into the individual folders because too large for github. 
 
 n_rand = 1 #want 2  randomizations for each tree size. internally each script will produce 500 random values. Can also do 1? 
 min_null_tree_size = 5
