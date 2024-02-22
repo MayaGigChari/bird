d03b058301df70b1241cc641c628d3b59598bb5d
Maya Chari <mchari@g.ucla.edu>
2024-02-21 22:17:59 -0800

    updated plant submission

--- Parallel_Hoffman_full/Hoffman_parallel_plant.R
+++ Parallel_Hoffman_full/Hoffman_parallel_plant.R
@@ -126,83 +126,83 @@ for(k in 1:num_instances)
   print(k)
   #start_time<-format(Sys.time(), "%H:%M:%S") #identify start time of script
   
-  #read in the tree data and the other thing. 
-  full_tree<-read.tree(file =  paste("~/bird/Parallel_Hoffman_full/", Clade, "/Instance_", k, "/cali_tree_interpolated.tre",sep = ""))
-  cophen_read<-readRDS(paste("~/bird/Parallel_Hoffman_full/", Clade, "/Instance_", k, "/cali_cophen_matrix",sep = ""))
-  
-  #could probably take this out of the loop but don't know how. 
-  sample_species<- sample(full_tree$tip.label, sample_size)
-  
-  #change this to make a community data matrix from the list of taxonomic names randomly sampled instead. 
-  
-  comm_sample<-commdata_maker(sample_species)
-  #this can be same for all the tree instantiations. 
-  
-
-
-  v <- c(rep(1, num_randomizations))
-
-  vectorized_stats_calculation<- function(v, coph_mat)
-  {
-    #comm_sample<-commdata_maker(sample_tree)
-    a<-coph_mat
-    mpd<- picante::mpd(comm_sample, picante:::taxaShuffle(a))
-    mntd<-picante::mntd(comm_sample, picante::taxaShuffle(a))
-    pd<-picante::pd(comm_sample,picante:::tipShuffle(full_tree))
-    r<-c(pd$PD,mpd,mntd)
-    names(r)<-c("pd","mpd","mntd")
-    l<-r
-    #v<-(picante::mpd(comm_sample, coph_mat)) 
-  }
-
-  stats<-lapply(v, vectorized_stats_calculation, coph_mat = cophen_read)
-
-
-  #prepare the data for export and export the data. 
-  temp <- unlist(stats[1])
-  if(num_randomizations>1)
-  {
-    for(i in 2:num_randomizations)
-    {
-      temp<-cbind(unlist(stats[i]), temp)
-    }
-  }
-  colnames(temp) = rep("trial", num_randomizations)
-  res_sample_cluster<-temp
-
-
-  #path_to_output <- paste("/u/home/m/mchari/bird/Parallel_Hoffman_full/", Clade, sep = "")
-  #path_csv<-file.path(path_to_output, folder)#trying to formally make file paths
-  #path_txt<-file.path(path_to_output,folder2)
-  
-  
-  folder_temp <- paste(path_to_output, "/", "Instance_", k, "/", a, "_output_files", sep = "")
-  #folder2_temp <- paste(path_to_output, "/","Instance_", k, "/", a, "_times", sep = "")
-  
-  print(folder_temp)
-  
-  path_csv<-file.path(folder_temp)#trying to formally make file paths
-  #path_txt<-file.path(folder2_temp)
-
-  fileIDcsv = paste(path_csv,"/",sample_size,"_",n, "out.csv", sep = '') #this fileID can be applied to all outputs.
-  #fileIDtimes = paste(path_txt,"/",sample_size,"_",n, "time.csv", sep = '') #this fileID can be applied to
-
-  #checkpoint
-  print(fileIDcsv)
-  print(fileIDtimes)
-
-  write.csv(res_sample_cluster, file = fileIDcsv, row.names = FALSE) #this i believe is the output file. I think it's maybe better than table?
-
-  #checkpoint
-  print("data output successful")
-  
-  #print the endtime for each run and add it to a csv file. 
-  #end_time<-format(Sys.time(), "%H:%M:%S") #identify end time of script
-  #times = c(start_time, end_time)
-  #point = c(1,2)
-  #df_times = data.frame (time = times, point = point)
-  #write.csv(df_times, file = fileIDtimes)
-  
+  # #read in the tree data and the other thing. 
+  # full_tree<-read.tree(file =  paste("~/bird/Parallel_Hoffman_full/", Clade, "/Instance_", k, "/cali_tree_interpolated.tre",sep = ""))
+  # cophen_read<-readRDS(paste("~/bird/Parallel_Hoffman_full/", Clade, "/Instance_", k, "/cali_cophen_matrix",sep = ""))
+  # 
+  # #could probably take this out of the loop but don't know how. 
+  # sample_species<- sample(full_tree$tip.label, sample_size)
+  # 
+  # #change this to make a community data matrix from the list of taxonomic names randomly sampled instead. 
+  # 
+  # comm_sample<-commdata_maker(sample_species)
+  # #this can be same for all the tree instantiations. 
+  # 
+  # 
+  # 
+  # v <- c(rep(1, num_randomizations))
+  # 
+  # vectorized_stats_calculation<- function(v, coph_mat)
+  # {
+  #   #comm_sample<-commdata_maker(sample_tree)
+  #   a<-coph_mat
+  #   mpd<- picante::mpd(comm_sample, picante:::taxaShuffle(a))
+  #   mntd<-picante::mntd(comm_sample, picante::taxaShuffle(a))
+  #   pd<-picante::pd(comm_sample,picante:::tipShuffle(full_tree))
+  #   r<-c(pd$PD,mpd,mntd)
+  #   names(r)<-c("pd","mpd","mntd")
+  #   l<-r
+  #   #v<-(picante::mpd(comm_sample, coph_mat)) 
+  # }
+  # 
+  # stats<-lapply(v, vectorized_stats_calculation, coph_mat = cophen_read)
+  # 
+  # 
+  # #prepare the data for export and export the data. 
+  # temp <- unlist(stats[1])
+  # if(num_randomizations>1)
+  # {
+  #   for(i in 2:num_randomizations)
+  #   {
+  #     temp<-cbind(unlist(stats[i]), temp)
+  #   }
+  # }
+  # colnames(temp) = rep("trial", num_randomizations)
+  # res_sample_cluster<-temp
+  # 
+  # 
+  # #path_to_output <- paste("/u/home/m/mchari/bird/Parallel_Hoffman_full/", Clade, sep = "")
+  # #path_csv<-file.path(path_to_output, folder)#trying to formally make file paths
+  # #path_txt<-file.path(path_to_output,folder2)
+  # 
+  # 
+  # folder_temp <- paste(path_to_output, "/", "Instance_", k, "/", a, "_output_files", sep = "")
+  # #folder2_temp <- paste(path_to_output, "/","Instance_", k, "/", a, "_times", sep = "")
+  # 
+  # print(folder_temp)
+  # 
+  # path_csv<-file.path(folder_temp)#trying to formally make file paths
+  # #path_txt<-file.path(folder2_temp)
+  # 
+  # fileIDcsv = paste(path_csv,"/",sample_size,"_",n, "out.csv", sep = '') #this fileID can be applied to all outputs.
+  # #fileIDtimes = paste(path_txt,"/",sample_size,"_",n, "time.csv", sep = '') #this fileID can be applied to
+  # 
+  # #checkpoint
+  # print(fileIDcsv)
+  # print(fileIDtimes)
+  # 
+  # write.csv(res_sample_cluster, file = fileIDcsv, row.names = FALSE) #this i believe is the output file. I think it's maybe better than table?
+  # 
+  # #checkpoint
+  # print("data output successful")
+  # 
+  # #print the endtime for each run and add it to a csv file. 
+  # #end_time<-format(Sys.time(), "%H:%M:%S") #identify end time of script
+  # #times = c(start_time, end_time)
+  # #point = c(1,2)
+  # #df_times = data.frame (time = times, point = point)
+  # #write.csv(df_times, file = fileIDtimes)
+  # 
 
 }
 
