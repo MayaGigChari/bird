af1ca58760fae6d93db6bea1d8a901a315c3fd9e
Maya Chari <mchari@g.ucla.edu>
2024-03-12 11:57:33 -0700

    updated files

--- Parallel_Hoffman_full/Cali_Geometry/Cali_ecoregions.R
+++ Parallel_Hoffman_full/Cali_Geometry/Cali_ecoregions.R
@@ -4,4 +4,5 @@ library(sf)
 
 ecoregion_shapefile <- st_read("Cali_geometry/ca_eco_l3/ca_eco_l3.shp") #this is an sf object of all the geometries in california. 
 
+plot(ecoregion_shapefile)
 #can join the ecoregion shapefiles by the california geometries/ ranges. 
